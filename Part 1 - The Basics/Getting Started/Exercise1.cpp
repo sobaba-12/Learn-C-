d864e712050c4e6b24948ae13809aa5298e40494
Mosobalaje Abayomi-Taiwo <sobataiwo@yahoo.com>
2024-09-20 19:01:48 +0100

    Initial commit

---
+++ Part 1 - The Basics/Getting Started/Exercise1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Soba on 20/09/2024.
+//
+
+# include <iostream>
+int main()
+{
+    int x = 8;
+    int y = 12;
+    std::cout << std::endl;
+    std::cout << x-y << " " << x*y << " " << x+y;
+    std::cout << std::endl;
+    return 0;
+}
---
+++ Part 1 - The Basics/The Anatomy of a C++ Program/DemoConsleOutput.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Soba on 20/09/2024.
+//
+
+#include <iostream>
+using namespace std;
+
+void demoConsoleOutput()
+{
+    cout << "This is a simple string literal" << endl;
+    cout << "Writing number five: " << 5 << endl;
+    cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
+    cout << "Pi when approximated is 22 / 7 = " << 22 / 7 << endl;
+    cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
+}
+
+int main()
+{
+    // Does the function and returns the value
+    return demoConsoleOutput();
+}
